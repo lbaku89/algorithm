0be385ab6062b331b0a130710e2b41332c63d50e
hyunwoo <khj930410@naver.com>
2024-12-20 14:32:05 +0900

    dfs -> recursive로 함수명 변경

--- baekjoon/silver/2/2630.js
+++ baekjoon/silver/2/2630.js
@@ -22,7 +22,7 @@ for (let i = 0; i < input.length; ++i) {
 let blue = 0;
 let white = 0;
 
-const dfs = (startR, startC, cnt) => {
+const recursive = (startR, startC, cnt) => {
     if (cnt === 1) {
         if (map[startR][startC] === 1) {
             blue++;
@@ -37,10 +37,10 @@ const dfs = (startR, startC, cnt) => {
         for (let j = startC; j < startC + cnt; ++j) {
             if (color !== map[i][j]) {
                 let half = cnt / 2;
-                dfs(startR, startC, half);
-                dfs(startR + half, startC, half);
-                dfs(startR, startC + half, half);
-                dfs(startR + half, startC + half, half);
+                recursive(startR, startC, half);
+                recursive(startR + half, startC, half);
+                recursive(startR, startC + half, half);
+                recursive(startR + half, startC + half, half);
                 return;
             }
         }
@@ -52,6 +52,6 @@ const dfs = (startR, startC, cnt) => {
     }
 };
 
-dfs(0, 0, N);
+recursive(0, 0, N);
 
 console.log(`${white}\n${blue}`);
