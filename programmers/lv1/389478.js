5d5fd76450f49297c654b2ace51164d846e77d3d
hyunwoo <khj930410@naver.com>
2025-02-25 23:31:55 +0900

    프로그래머스 > 택배상자꺼내기기

--- programmers/lv1/389478.js
+++ programmers/lv1/389478.js
@@ -1,5 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/389478
-// 최적화 필요
+// 최적화 필요 - 택배상자 꺼내기
 function solution(n, w, num) {
     let map = [];
     let temp = [];
@@ -59,3 +59,44 @@ function solution(n, w, num) {
 
     return answer;
 }
+
+// function solution(n, w, num) {
+//     let a = Math.floor((n - 1) / w);
+//     let b = Math.floor((num - 1) / w);
+
+//     let cnt = undefined;
+//     if (a === b) {
+//         // 가장 윗줄
+//         return 1;
+//     } else {
+//         // 다른 줄
+//         cnt = a - b;
+
+//         // num은 박스 해당줄의 왼쪽으로부터 몇번째인지
+//         let numIdx = undefined;
+//         if (Math.ceil(num / w) % 2 === 0) {
+//             // 짝수
+//             numIdx = w - ((num - 1) % w);
+//         } else {
+//             // 홀수
+//             numIdx = num % w;
+//         }
+
+//         // 마지막 n번 박스가 해당줄 왼쪽으로 부터 몇번째인지
+//         let nIdx = undefined;
+//         if (Math.ceil(n / w) % 2 === 0) {
+//             //짝수
+//             nIdx = w - ((n - 1) % w);
+//             if (nIdx <= numIdx) {
+//                 ++cnt;
+//             }
+//         } else {
+//             //홀수번째
+//             nIdx = n % w;
+//             if (nIdx >= numIdx) {
+//                 ++cnt;
+//             }
+//         }
+//     }
+//     return cnt;
+// }
