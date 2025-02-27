038f75223b15740d615efd556c62091832b513d3
hyunwoo <khj930410@naver.com>
2025-02-27 23:54:55 +0900

    백준 9375번 시도 ( 실패 -> 디버깅 필요 )

---
+++ baekjoon/silver/3/9375.js
@@ -0,0 +1,55 @@
+// https://www.acmicpc.net/problem/9375
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let testCase = "2\n3\nhat headgear\nsunglasses eyewear\nturban headgear\n3\nmask face\nsunglasses face\nmakeup face";
+let input = testCase.toString().trim().split("\n");
+
+let answer = "";
+for (let i = 1; i < input.length; ) {
+    let obj = {};
+    let clothCount = +input[i];
+
+    for (let j = 1; j <= clothCount; ++j) {
+        let [cloth, type] = input[i + j].split(" ");
+
+        if (obj[type] === undefined) {
+            obj[type] = 1;
+        } else {
+            ++obj[type];
+        }
+    }
+
+    console.log("obj>", obj);
+    let typeArr = Object.keys(obj);
+    console.log("typeArr>", typeArr);
+    let typeCount = Object.keys(obj).length;
+    let mixArr = [];
+
+    let temp = 0;
+    const mix = (start, restPickCnt, pickedArr) => {
+        if (restPickCnt === 0) {
+            console.log("pickedArr>", pickedArr);
+            let cnt = 1;
+            pickedArr.forEach((type) => {
+                cnt = cnt * obj[type];
+            });
+            temp += cnt;
+        }
+
+        for (let z = start; z < typeArr.length; ++z) {
+            pickedArr.push(typeArr[z]);
+            mix(start + 1, restPickCnt - 1, pickedArr);
+            pickedArr.pop();
+        }
+    };
+
+    for (let k = 1; k <= typeArr.length; ++k) {
+        mix(0, k, []);
+    }
+
+    answer += `${temp}\n`;
+
+    i += clothCount + 1;
+}
+console.log(answer);
--- programmers/lv2/12902.js
+++ programmers/lv2/12902.js
@@ -1,6 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12902
 // 3 x n 타일링
-
+// https://velog.io/@longroadhome/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.4-3-x-n-%ED%83%80%EC%9D%BC%EB%A7%81-JS
 function solution(n) {
     // n은 5,000 이하
 
