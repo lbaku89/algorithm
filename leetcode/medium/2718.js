e68d8ffcb6b6a1e2ddc2f3b3802d59fb9c9b3e4e
hyunwoo <khj930410@naver.com>
2025-03-15 19:36:37 +0900

    leetCode 2718번

--- leetcode/medium/2718.js
+++ leetcode/medium/2718.js
@@ -1,5 +1,34 @@
 // https://leetcode.com/problems/sum-of-matrix-after-queries/
 
+// 성공 good!
+/**
+ * @param {number} n
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var matrixSumQueries = function (n, queries) {
+    let sum = 0;
+
+    let removedRow = new Set();
+    let removedColumn = new Set();
+
+    for (let i = queries.length - 1; i >= 0; --i) {
+        let [type, index, val] = queries[i];
+
+        if (type && !removedColumn.has(index)) {
+            // 1 -> column
+            removedColumn.add(index);
+            sum += (n - removedRow.size) * val;
+        } else if (!type && !removedRow.has(index)) {
+            // 0 -> row
+            removedRow.add(index);
+            sum += (n - removedColumn.size) * val;
+        }
+    }
+
+    return sum;
+};
+
 // FATAL ERROR: Reached heap limit allocation failed
 /**
  * @param {number} n
