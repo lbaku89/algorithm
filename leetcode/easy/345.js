9cc24394bc12020f70169c7b33afbcbce9b1c048
hyunwoo <khj930410@naver.com>
2025-05-07 23:32:47 +0900

    feat: other solution for LeetCode problem 345

--- leetcode/easy/345.js
+++ leetcode/easy/345.js
@@ -39,5 +39,49 @@ var reverseVowels = function (s) {
     return str.join("");
 };
 
+// other solution
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    let str = s.split("");
+
+    const isVowel = (cha) => {
+        if (cha === " ") {
+            return false;
+        }
+        switch (cha.toLowerCase()) {
+            case "a":
+                return true;
+            case "e":
+                return true;
+            case "i":
+                return true;
+            case "o":
+                return true;
+            case "u":
+                return true;
+            default:
+                return false;
+        }
+    };
+
+    let [left, right] = [0, str.length - 1];
+
+    while (left < right) {
+        if (isVowel(str[left])) {
+            while (!isVowel(str[right])) {
+                --right;
+            }
+            [str[left], str[right]] = [str[right], str[left]];
+            --right;
+        }
+        ++left;
+    }
+
+    return str.join("");
+};
+
 // testCase
 console.log(reverseVowels("IceCreAm"));
