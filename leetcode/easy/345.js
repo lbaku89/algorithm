c74d14b145a7956f9dd1130d41f8a937e9d89442
hyunwoo <khj930410@naver.com>
2025-05-07 23:22:09 +0900

    feat: solve LeetCode problem 345

---
+++ leetcode/easy/345.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/reverse-vowels-of-a-string/?envType=study-plan-v2&envId=leetcode-75
+
+// time complexity O(n)
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    let str = s.split("");
+
+    const isVowel = (cha) => {
+        if (cha === " ") {
+            return false;
+        }
+        switch (cha.toLowerCase()) {
+            case "a":
+                return true;
+            case "e":
+                return true;
+            case "i":
+                return true;
+            case "o":
+                return true;
+            case "u":
+                return true;
+            default:
+                return false;
+        }
+    };
+    let arr = str.filter((v) => isVowel(v));
+
+    let idx = arr.length - 1;
+    for (let i = 0; i < str.length; ++i) {
+        if (isVowel(str[i])) {
+            str[i] = arr[idx];
+            --idx;
+        }
+    }
+    return str.join("");
+};
+
+// testCase
+console.log(reverseVowels("IceCreAm"));
