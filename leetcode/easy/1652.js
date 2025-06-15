a08bc8ff37dbd77bd47b2cc2bd7814b428c42412
hyunwoo <khj930410@naver.com>
2025-06-15 18:06:51 +0900

    feat: solve LeetCode problem 1652 ( optimization )

--- leetcode/easy/1652.js
+++ leetcode/easy/1652.js
@@ -1,5 +1,49 @@
 // https://leetcode.com/problems/defuse-the-bomb/
 
+// other solution - sliding window
+
+/**
+ * @param {number[]} code
+ * @param {number} k
+ * @return {number[]}
+ */
+var decrypt = function (code, k) {
+    if (k === 0) {
+        return new Array(code.length).fill(0);
+    }
+
+    let ref = code.concat(code);
+    let arr = new Array(code.length).fill(0);
+
+    if (k > 0) {
+        let sum = 0;
+        for (let i = 0; i <= k - 1; ++i) {
+            sum += ref[i];
+        }
+
+        for (let i = 0; i < code.length; ++i) {
+            sum = sum - ref[i] + ref[k + i];
+            arr[i] = sum;
+        }
+    }
+
+    if (k < 0) {
+        let sum = 0;
+
+        for (let i = code.length - 1; i >= code.length + k; --i) {
+            sum += ref[i];
+        }
+        arr[0] = sum;
+
+        for (let i = code.length + 1; i < ref.length; ++i) {
+            sum = sum - ref[i + k - 1] + ref[i - 1];
+            arr[i - code.length] = sum;
+        }
+    }
+
+    return arr;
+};
+
 // time complexity O(n * |k|)
 /**
  * @param {number[]} code
