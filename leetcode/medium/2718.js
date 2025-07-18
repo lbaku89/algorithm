807c3ffc08f6583cbdb778556da98ba704a12c60
hyunwoo <khj930410@naver.com>
2025-07-18 23:29:52 +0900

    feat: solve LeetCode problem 2718

--- leetcode/medium/2718.js
+++ leetcode/medium/2718.js
@@ -1,6 +1,40 @@
 // https://leetcode.com/problems/sum-of-matrix-after-queries/
 
 // 성공 good!
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var matrixSumQueries = function(n, queries) {
+
+    let rows = Array(n).fill(true)
+    let validRows = n
+    let cols = Array(n).fill(true)
+    let validCols = n
+    let sum =0
+    for(let i=queries.length-1; i>=0; --i ){
+        let [type,index,val] = queries[i]
+        
+        if(type ===1){
+            if(cols[index]){
+                sum += (validRows*val)
+                cols[index] = false
+                validCols = Math.max(0,validCols-1) 
+            }
+        }else{
+            if(rows[index]){
+                sum += (validCols*val)
+                rows[index] = false
+                validRows = Math.max(0,validRows-1) 
+            }
+        }
+    }   
+
+    return sum
+};
 /**
  * @param {number} n
  * @param {number[][]} queries
