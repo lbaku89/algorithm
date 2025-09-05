dfba29517e339acf2a01301b8c6d72e3da42ed7b
hyunwoo <khj930410@naver.com>
2025-09-05 23:34:24 +0900

    feat: solve LeetCode problem 2621

---
+++ leetcode/easy/2621.js
@@ -0,0 +1,20 @@
+/**
+ * @problem https://leetcode.com/problems/sleep/
+ */
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, millis);
+  });
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
