fb5f4febf0ed37951706388ae668fac4dfa2acfe
hyunwoo <khj930410@naver.com>
2025-09-10 00:47:02 +0900

    feat: optimization solution

--- codility/medium/training6ASY2A-8AE.js
+++ codility/medium/training6ASY2A-8AE.js
@@ -17,3 +17,34 @@ function solution(N, A) {
 
   return answer.slice(1, answer.length);
 }
+
+/**
+ * @optimization
+ */
+
+function solution(N, A) {
+  let answer = Array(N + 1).fill(0);
+  let base = 0;
+  let maxNum = 0;
+  A.forEach((x, k) => {
+    if (x >= 1 && x <= N) {
+      if (answer[x] < base) {
+        answer[x] = base + 1;
+      } else {
+        ++answer[x];
+      }
+
+      maxNum = Math.max(maxNum, answer[x]);
+    } else if (x === N + 1) {
+      base = maxNum;
+    }
+  });
+
+  answer.forEach((n, idx) => {
+    if (n < base) {
+      answer[idx] = base;
+    }
+  });
+
+  return answer.slice(1, answer.length);
+}
