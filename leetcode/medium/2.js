2ae8b2b87b18d74c6c3dcad8cca690bffad4f6d7
hyunwoo <khj930410@naver.com>
2025-09-14 13:49:46 +0900

    feat: optimize solution ( LeetCode Problem 2 )

--- leetcode/medium/2.js
+++ leetcode/medium/2.js
@@ -3,6 +3,47 @@
  */
 
 /**
+ * @optimize
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  const answerNode = new ListNode();
+  let curNode = answerNode;
+  let addOne = false;
+  while (l1 || l2 || addOne) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (addOne ? 1 : 0);
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+    if (sum >= 10) {
+      addOne = true;
+    } else {
+      addOne = false;
+    }
+
+    curNode.val = sum % 10;
+
+    if (l1 || l2 || addOne) {
+      curNode.next = new ListNode();
+      curNode = curNode.next;
+    }
+  }
+
+  return answerNode;
+};
+
+/**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
