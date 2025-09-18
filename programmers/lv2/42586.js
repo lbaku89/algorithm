5ca6b6792ca733835a74e9a02454071b657f2b29
hyunwoo <khj930410@naver.com>
2025-09-18 23:24:39 +0900

    feat: optimize readability

--- programmers/lv2/42586.js
+++ programmers/lv2/42586.js
@@ -3,6 +3,33 @@
  */
 
 function solution(progresses, speeds) {
+  const days = progresses.map((progress, idx) => {
+    return Math.ceil((100 - progress) / speeds[idx]);
+  });
+
+  const answer = [];
+
+  let cnt = 0;
+  let standard = days[0];
+
+  for (let i = 0; i < days.length; ++i) {
+    if (standard >= days[i]) {
+      ++cnt;
+    } else {
+      answer.push(cnt);
+      standard = days[i];
+      cnt = 1;
+    }
+  }
+
+  if (cnt) {
+    answer.push(cnt);
+  }
+
+  return answer;
+}
+
+function solution(progresses, speeds) {
   let answer = [];
   let i = 0;
 
