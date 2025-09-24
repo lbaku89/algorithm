3e0dbb0f6b9a029dc9d97874e2efd2561367f6db
hyunwoo <khj930410@naver.com>
2025-09-24 23:13:33 +0900

    refactor: optimization problem 349 by using Set Object

--- leetcode/easy/349.js
+++ leetcode/easy/349.js
@@ -1,28 +1,46 @@
 /**
- * @time complexity O(n)  { num1.length  + num2.length ...}
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-  const dictionary = {};
-  for (const num of nums1) {
-    dictionary[num] = 1;
-  }
+  const set = new Set(nums1);
+  const resultSet = new Set();
 
-  const answer = [];
-  const alreadyPushed = {};
-
-  for (const num of nums2) {
-    if (alreadyPushed[num]) {
-      continue;
-    }
-
-    if (dictionary[num]) {
-      answer.push(num);
-      alreadyPushed[num] = 1;
+  for (const n of nums2) {
+    if (set.has(n)) {
+      resultSet.add(n);
     }
   }
 
-  return answer;
+  return [...resultSet];
 };
+
+/**
+ * @time complexity O(n)  { num1.length  + num2.length ...}
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+// var intersection = function (nums1, nums2) {
+//   const dictionary = {};
+//   for (const num of nums1) {
+//     dictionary[num] = 1;
+//   }
+
+//   const answer = [];
+//   const alreadyPushed = {};
+
+//   for (const num of nums2) {
+//     if (alreadyPushed[num]) {
+//       continue;
+//     }
+
+//     if (dictionary[num]) {
+//       answer.push(num);
+//       alreadyPushed[num] = 1;
+//     }
+//   }
+
+//   return answer;
+// };
