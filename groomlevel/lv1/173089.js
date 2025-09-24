f196ca738e35a58f3a90f36a7bca1bde6e9cce9c
hyunwoo <khj930410@naver.com>
2025-09-25 00:59:58 +0900

    feat: solve groomlevel problem 173089

---
+++ groomlevel/lv1/173089.js
@@ -0,0 +1,14 @@
+// Run by Node.js
+const readline = require("readline");
+
+(async () => {
+  let rl = readline.createInterface({ input: process.stdin });
+
+  for await (const line of rl) {
+    console.log(String(line).length);
+
+    rl.close();
+  }
+
+  process.exit();
+})();
