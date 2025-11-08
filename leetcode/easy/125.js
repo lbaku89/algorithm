cf9c597296b74b097c8e242ce3faee660e49f7ad
hyunwoo <khj930410@naver.com>
2025-11-08 17:55:12 +0900

    feat: other solution for LeetCode problem 125

--- leetcode/easy/125.js
+++ leetcode/easy/125.js
@@ -3,8 +3,49 @@
  */
 
 /**
+ * O(n)
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const isLetter = (letter) => {
+    const code = letter.charCodeAt(0);
+    if ("a".charCodeAt(0) <= code && code <= "z".charCodeAt(0)) {
+      return true;
+    } else if ("A".charCodeAt(0) <= code && code <= "Z".charCodeAt(0)) {
+      return true;
+    } else if ("0".charCodeAt(0) <= code && code <= "9".charCodeAt(0)) {
+      return true;
+    }
+    return false;
+  };
+
+  let [leftIdx, rightIdx] = [0, s.length - 1];
+
+  while (leftIdx < rightIdx) {
+    while (leftIdx < rightIdx && !isLetter(s[leftIdx])) {
+      ++leftIdx;
+    }
+    while (leftIdx < rightIdx && !isLetter(s[rightIdx])) {
+      --rightIdx;
+    }
+
+    if (
+      leftIdx < rightIdx &&
+      s[leftIdx].toLowerCase() !== s[rightIdx].toLowerCase()
+    ) {
+      return false;
+    }
+    ++leftIdx;
+    --rightIdx;
+  }
+
+  return true;
+};
+
+/**
  *
- *
+ *O(n)
  * @param {string} s
  * @return {boolean}
  */
