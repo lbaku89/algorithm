d4c6618da86eb48b7f4104b51547f339dbfba3e8
hyunwoo <khj930410@naver.com>
2025-11-08 17:31:34 +0900

    feat: solve LeetCode problem 125

---
+++ leetcode/easy/125.js
@@ -0,0 +1,42 @@
+/**
+ * @problem https://chatgpt.com/c/690efec2-5a4c-8324-947e-e3f5227ae890
+ */
+
+/**
+ *
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const isLetter = (letter) => {
+    const code = letter.charCodeAt(0);
+
+    if ("a".charCodeAt(0) <= code && code <= "z".charCodeAt(0)) {
+      return true;
+    }
+
+    if ("A".charCodeAt(0) <= code && code <= "Z".charCodeAt(0)) {
+      return true;
+    }
+
+    if ("0".charCodeAt(0) <= code && code <= "9".charCodeAt(0)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const arr = [...s]
+    .filter((cha) => isLetter(cha))
+    .map((cha) => cha.toLowerCase());
+
+  const n = arr.length - 1;
+  const end = Math.floor(arr.length / 2) - 1;
+  for (let i = 0; i <= end; ++i) {
+    if (arr[i] !== arr[n - i]) {
+      return false;
+    }
+  }
+  return true;
+};
