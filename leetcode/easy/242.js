2849d8acf38c0589e76657615fbc61efb5aa8563
hyunwoo <khj930410@naver.com>
2025-11-08 13:11:29 +0900

    feat: other solution for LeetCode 242 problem

--- leetcode/easy/242.js
+++ leetcode/easy/242.js
@@ -7,6 +7,31 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const countMap = Array(26).fill(0);
+  for (let i = 0; i < s.length; ++i) {
+    ++countMap[s.charCodeAt(i) - 97];
+    --countMap[t.charCodeAt(i) - 97];
+  }
+
+  for (const cnt of countMap) {
+    if (cnt !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+/**
+ * O(n)
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
  *
  */
 var isAnagram = function (s, t) {
