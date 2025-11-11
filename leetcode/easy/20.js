b30a3652bd97f576563c86114ff8ef36e49d7fb4
hyunwoo <khj930410@naver.com>
2025-11-11 22:36:54 +0900

    feat: solve LeetCode problem 20

--- leetcode/easy/20.js
+++ leetcode/easy/20.js
@@ -1,26 +1,55 @@
 // time complexity O(n)
+// space complexity O(n)
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function (s) {
-    let arr = [];
-
-    const openFlag = ["(", "{", "["];
-    const closeFlag = [")", "}", "]"];
-    for (const cha of [...s]) {
-        let idx = openFlag.indexOf(cha);
-        let idx2 = closeFlag.indexOf(cha);
-        if (idx >= 0) {
-            arr.push(cha);
-        } else {
-            if (arr.at(-1) !== openFlag[idx2]) {
-                return false;
-            } else {
-                arr.pop();
-            }
-        }
+  const map = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
+  const stack = [];
+  for (const bracket of s) {
+    if (map[bracket]) {
+      const popped = stack.pop();
+      if (map[bracket] !== popped) {
+        return false;
+      }
+    } else {
+      stack.push(bracket);
     }
+  }
 
-    return arr.length === 0 ? true : false;
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let arr = [];
+
+  const openFlag = ["(", "{", "["];
+  const closeFlag = [")", "}", "]"];
+  for (const cha of [...s]) {
+    let idx = openFlag.indexOf(cha);
+    let idx2 = closeFlag.indexOf(cha);
+    if (idx >= 0) {
+      arr.push(cha);
+    } else {
+      if (arr.at(-1) !== openFlag[idx2]) {
+        return false;
+      } else {
+        arr.pop();
+      }
+    }
+  }
+
+  return arr.length === 0 ? true : false;
 };
