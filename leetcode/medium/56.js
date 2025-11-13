f6dfb8c37650405b6433c3b846c45f866a3c75c5
hyunwoo <khj930410@naver.com>
2025-11-13 22:08:45 +0900

    feat: solve LeetCode problem 56

--- leetcode/medium/56.js
+++ leetcode/medium/56.js
@@ -1,39 +1,27 @@
 /**@origin https://leetcode.com/problems/merge-intervals/ */
 
 /**
- * @description time-complexity O(nlogn)
+ * @timeComplexity O(nlogn)
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
+var merge = function (intervals) {
+  intervals.sort((prev, next) => {
+    return prev[0] - next[0] || prev[1] - next[1];
+  });
+  const answer = [];
+  answer.push(intervals[0]);
 
-    // O(nlogn)
-    intervals.sort((a,b)=>{
-        return a[0]-b[0]
-    })
+  for (let i = 1; i < intervals.length; ++i) {
+    const last = answer[answer.length - 1];
+    const [start, end] = intervals[i];
 
-    let answer = []
-    let temp = [intervals[0][0],intervals[0][1]]
-    
-    // O(n) 
-    for(let i=0; i<intervals.length; ++i){
-        
-        const [s,e] = intervals[i]
-
-        if(s<=temp[1]){
-            temp[1]=Math.max(e,temp[1])
-        }else{
-            answer.push([...temp])
-            temp[0]=s
-            temp[1]=e
-        }
-
-
-        if(i===intervals.length-1){
-            answer.push([...temp])
-        }
+    if (start <= last[1]) {
+      last[1] = Math.max(last[1], end);
+    } else {
+      answer.push([start, end]);
     }
-    
-    return answer
-    
+  }
+
+  return answer;
 };
