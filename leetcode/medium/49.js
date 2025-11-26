558000920af2c0ae14d1e89220c5bae303cc2d45
hyunwoo <khj930410@naver.com>
2025-11-27 00:04:41 +0900

    feat: solve LeetCode problem 49 (group anagrams)

--- leetcode/medium/49.js
+++ leetcode/medium/49.js
@@ -2,6 +2,39 @@
  * @origin https://leetcode.com/problems/group-anagrams/description/
  */
 
+// 제한 조건
+// ( n = strs.length , m = max of strs[i] )
+// 1 <= strs.length <= 10**4
+// 0 <= strs[i].length <= 100
+
+// space complexity O(n * m)
+// time complexity O(n * m*log m)
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  // 공간복잡도 n * m
+  const map = {};
+
+  // 시간 복잡도 strs.length
+  strs.forEach((str, idx) => {
+    // str.length = m
+    // 시간 복잡도 m + m * logm + m  , 공간 복잡도  + 1
+    const key = [...str].sort().join("");
+
+    if (!map[key]) {
+      // 시간 복잡도 +1
+      map[key] = [strs[idx]];
+    } else {
+      // 시간 복잡도 +1
+      map[key].push(strs[idx]);
+    }
+  });
+
+  return Object.values(map);
+};
 /**
  * @param {string[]} strs
  * @return {string[][]}
