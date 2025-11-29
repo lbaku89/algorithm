d6e00c4f3559d4aede452fa2d8a29cbd15464b23
hyunwoo <khj930410@naver.com>
2025-11-30 00:42:26 +0900

    feat: solve LeetCode problem 739 (daily temperatures)

--- leetcode/medium/739.js
+++ leetcode/medium/739.js
@@ -1,6 +1,45 @@
+// https://leetcode.com/problems/daily-temperatures/
+
 // time complexity O(n)
 // space complexity O(n)
 // monotonic stack
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+
+var dailyTemperatures = function (temperatures) {
+  const arr = Array(101).fill(false);
+  const answer = Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; ++i) {
+    const temp = temperatures[i];
+
+    if (!arr[temp]) {
+      arr[temp] = [i];
+    } else {
+      arr[temp].push(i);
+    }
+
+    if (i === 0) {
+      continue;
+    }
+
+    for (let j = 30; j < temp; ++j) {
+      if (!arr[j]) {
+        continue;
+      }
+
+      arr[j].forEach((idx) => {
+        answer[idx] = i - idx;
+      });
+      arr[j] = false;
+    }
+  }
+
+  return answer;
+};
 
 /**
  *
