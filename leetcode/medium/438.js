fdf548b664285bf87e010c4e30c5c4dd1cb3bd08
hyunwoo <khj930410@naver.com>
2025-12-13 00:04:15 +0900

    feat: other solution for LeetCode problem 438

--- leetcode/medium/438.js
+++ leetcode/medium/438.js
@@ -1,6 +1,57 @@
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
 
 /**
+ * O(n)
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function (s, p) {
+  if (s.length < p.length) {
+    return [];
+  }
+
+  const answer = [];
+  const aCode = "a".charCodeAt(0);
+  const zCode = "z".charCodeAt(0);
+  const l = zCode - aCode + 1;
+
+  const pArr = Array(l).fill(0);
+  const windowArr = Array(l).fill(0);
+
+  p.split("").forEach((ch, idx) => {
+    ++pArr[ch.charCodeAt(0) - 97];
+    ++windowArr[s[idx].charCodeAt(0) - 97];
+  });
+
+  const isAnagram = () => {
+    for (let i = 0; i < pArr.length; ++i) {
+      if (pArr[i] !== windowArr[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  if (isAnagram()) {
+    answer.push(0);
+  }
+
+  for (let right = p.length; right < s.length; ++right) {
+    const enteringIdx = s[right].charCodeAt(0) - 97;
+    const leavingIdx = s[right - p.length].charCodeAt(0) - 97;
+    ++windowArr[enteringIdx];
+    --windowArr[leavingIdx];
+
+    if (isAnagram()) {
+      answer.push(right - p.length + 1);
+    }
+  }
+
+  return answer;
+};
+
+// ------------------------------------------------------------
+/**
  * @param {string} s
  * @param {string} p
  * @return {number[]}
