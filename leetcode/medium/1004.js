19d450b1b5f2c8397cf7146b4d0902262e8ed05b
hyunwoo <khj930410@naver.com>
2025-12-13 15:14:56 +0900

    feat: optimized solution for LeetCode Problem 1004

--- leetcode/medium/1004.js
+++ leetcode/medium/1004.js
@@ -1,5 +1,34 @@
 // https://leetcode.com/problems/max-consecutive-ones-iii/?envType=study-plan-v2&envId=leetcode-75
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+  let left = 0;
+  let zeroCnt = 0;
+  let answer = 0;
+
+  for (let right = 0; right < nums.length; ++right) {
+    const n = nums[right];
+    if (n === 0) {
+      ++zeroCnt;
+    }
+
+    while (zeroCnt > k) {
+      if (nums[left] === 0) {
+        --zeroCnt;
+      }
+      ++left;
+    }
+
+    answer = Math.max(answer, right - left + 1);
+  }
+
+  return answer;
+};
+
 // time complexity O(n)
 /**
  * @param {number[]} nums
@@ -7,50 +36,50 @@
  * @return {number}
  */
 var longestOnes = function (nums, k) {
-    let oneCnt = 0;
-    let flipped = 0;
-    let max = 0;
-    for (let i = 0; i < nums.length; ) {
-        let n = nums[i];
+  let oneCnt = 0;
+  let flipped = 0;
+  let max = 0;
+  for (let i = 0; i < nums.length; ) {
+    let n = nums[i];
 
-        if (n) {
-            // 1일때
-            ++oneCnt;
-        } else {
-            // 0일때
-            ++flipped;
+    if (n) {
+      // 1일때
+      ++oneCnt;
+    } else {
+      // 0일때
+      ++flipped;
 
-            let first = nums[i - oneCnt];
-            if (flipped > k) {
-                // 뒤집은 갯수 최대를 넘을때
+      let first = nums[i - oneCnt];
+      if (flipped > k) {
+        // 뒤집은 갯수 최대를 넘을때
 
-                if (first) {
-                    let idx = i - oneCnt;
+        if (first) {
+          let idx = i - oneCnt;
 
-                    while (1) {
-                        if (nums[idx] === 0) {
-                            break;
-                        }
-                        --oneCnt;
-                        ++idx;
-                    }
-                    //1일때
-                } else {
-                    //0일때
-                    oneCnt = oneCnt;
-                    flipped = k;
-                }
-            } else {
-                // 뒤집은 갯수 최대를 넘지 않았을때
-                ++oneCnt;
+          while (1) {
+            if (nums[idx] === 0) {
+              break;
             }
+            --oneCnt;
+            ++idx;
+          }
+          //1일때
+        } else {
+          //0일때
+          oneCnt = oneCnt;
+          flipped = k;
         }
-
-        // console.log("i", i, "oneCnt", oneCnt);
-        max = Math.max(max, oneCnt);
-        ++i;
+      } else {
+        // 뒤집은 갯수 최대를 넘지 않았을때
+        ++oneCnt;
+      }
     }
-    return max;
+
+    // console.log("i", i, "oneCnt", oneCnt);
+    max = Math.max(max, oneCnt);
+    ++i;
+  }
+  return max;
 };
 
 // other solution for sliding window
@@ -61,31 +90,39 @@ var longestOnes = function (nums, k) {
  * @return {number}
  */
 var longestOnes = function (nums, k) {
-    let left = 0;
-    let zeroCnt = 0;
-    let max = 0;
-    for (let right = 0; right < nums.length; ++right) {
-        if (nums[right] === 0) {
-            ++zeroCnt;
-        }
-
-        if (zeroCnt > k) {
-            while (nums[left] !== 0) {
-                ++left;
-            }
-            ++left;
-            --zeroCnt;
-        }
-
-        let cnt = right - left + 1;
-        max = Math.max(cnt, max);
+  let left = 0;
+  let zeroCnt = 0;
+  let max = 0;
+  for (let right = 0; right < nums.length; ++right) {
+    if (nums[right] === 0) {
+      ++zeroCnt;
     }
-    return max;
+
+    if (zeroCnt > k) {
+      while (nums[left] !== 0) {
+        ++left;
+      }
+      ++left;
+      --zeroCnt;
+    }
+
+    let cnt = right - left + 1;
+    max = Math.max(cnt, max);
+  }
+  return max;
 };
 
 // testCase
 console.log(
-    longestOnes([1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1], 8)
+  longestOnes(
+    [
+      1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1,
+      1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1,
+    ],
+    8
+  )
 ); //25
 console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); //6
-console.log(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)); //10
+console.log(
+  longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)
+); //10
