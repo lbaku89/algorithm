754aa19ca086e84c176e35a3c699a87f55bc7894
hyunwoo <khj930410@naver.com>
2026-02-22 00:13:04 +0900

    refactor: improve performance of solution for LeetCode problem 1365 by using a counting array and dynamic programming

--- leetcode/easy/1365.ts
+++ leetcode/easy/1365.ts
@@ -2,18 +2,34 @@
 // time complexity O(n)
 
 function smallerNumbersThanCurrent(nums: number[]): number[] {
-  const count = new Array(101).fill(0);
+  // dp[idx] 는 idx 보다 작은 숫자 갯수
+  // dp[idx] = dp[idx-1] + cnt[idx-1]
 
-  for (const n of nums) {
-    count[n]++;
-  }
-
-  for (let i = 1; i < 101; i++) {
-    count[i] += count[i - 1];
-  }
-
-  return nums.map((n) => {
-    if (n === 0) return 0;
-    return count[n - 1];
+  const numsCount = Array(101).fill(0);
+  nums.forEach((n) => {
+    ++numsCount[n];
   });
+
+  const dp = Array(101).fill(0);
+  for (let i = 1; i <= 100; ++i) {
+    dp[i] = dp[i - 1] + numsCount[i - 1];
+  }
+
+  return nums.map((n) => dp[n]);
 }
+// function smallerNumbersThanCurrent(nums: number[]): number[] {
+//   const count = new Array(101).fill(0);
+
+//   for (const n of nums) {
+//     count[n]++;
+//   }
+
+//   for (let i = 1; i < 101; i++) {
+//     count[i] += count[i - 1];
+//   }
+
+//   return nums.map((n) => {
+//     if (n === 0) return 0;
+//     return count[n - 1];
+//   });
+// }
