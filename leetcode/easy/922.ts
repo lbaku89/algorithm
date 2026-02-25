3b200658d9e3ea4426169dd0f1281bb1f2ad32ff
hyunwoo <khj930410@naver.com>
2026-02-25 23:50:01 +0900

    refactor: optimize solution for LeetCode problem 922 by using a single pass approach to arrange even and odd numbers

--- leetcode/easy/922.ts
+++ leetcode/easy/922.ts
@@ -1,23 +1,44 @@
+//https://leetcode.com/problems/sort-array-by-parity-ii/description/
 function sortArrayByParityII(nums: number[]): number[] {
-  const evenNum: number[] = [];
-  const oddNum: number[] = [];
+  const answer: number[] = Array(nums.length).fill(0);
 
-  nums.forEach((n: number) => {
+  let oddIdx = 1;
+  let evenIdx = 0;
+  let idx = 0;
+  while (idx < nums.length) {
+    const n = nums[idx];
     if (n % 2 === 0) {
-      evenNum.push(n);
+      answer[evenIdx] = n;
+      evenIdx += 2;
     } else {
-      oddNum.push(n);
+      answer[oddIdx] = n;
+      oddIdx += 2;
     }
-  });
-
-  const answer: number[] = [];
-  for (let i = 0; i < nums.length; ++i) {
-    if (i % 2 === 0) {
-      answer.push(evenNum.pop() as number);
-    } else {
-      answer.push(oddNum.pop() as number);
-    }
+    ++idx;
   }
-
   return answer;
 }
+
+// function sortArrayByParityII(nums: number[]): number[] {
+//   const evenNum: number[] = [];
+//   const oddNum: number[] = [];
+
+//   nums.forEach((n: number) => {
+//     if (n % 2 === 0) {
+//       evenNum.push(n);
+//     } else {
+//       oddNum.push(n);
+//     }
+//   });
+
+//   const answer: number[] = [];
+//   for (let i = 0; i < nums.length; ++i) {
+//     if (i % 2 === 0) {
+//       answer.push(evenNum.pop() as number);
+//     } else {
+//       answer.push(oddNum.pop() as number);
+//     }
+//   }
+
+//   return answer;
+// }
