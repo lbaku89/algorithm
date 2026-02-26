0aabd4c5a67648958440406a5dff78b6399a7296
hyunwoo <khj930410@naver.com>
2026-02-26 20:51:04 +0900

    refactor: optimize solution

--- leetcode/easy/700.ts
+++ leetcode/easy/700.ts
@@ -26,21 +26,60 @@ class TreeNode {
 }
 
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
-  let answer: TreeNode | null = null;
-  const dfs = (r: TreeNode) => {
-    if (r.val === val) {
-      answer = r;
-      return;
-    }
+  if (root?.val === val) {
+    return root;
+  }
 
-    if (r.left) {
-      dfs(r.left);
-    }
-    if (r.right) {
-      dfs(r.right);
-    }
-  };
-  dfs(root!);
+  if (root?.left && root.val > val) {
+    return searchBST(root.left, val);
+  } else if (root?.right && root.val < val) {
+    return searchBST(root.right, val);
+  }
 
-  return answer;
+  return null;
 }
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// class TreeNode {
+//   val: number;
+//   left: TreeNode | null;
+//   right: TreeNode | null;
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.left = left === undefined ? null : left;
+//     this.right = right === undefined ? null : right;
+//   }
+// }
+
+// function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+//   let answer: TreeNode | null = null;
+//   const dfs = (r: TreeNode) => {
+//     if (r.val === val) {
+//       answer = r;
+//       return;
+//     }
+
+//     if (r.left) {
+//       dfs(r.left);
+//     }
+//     if (r.right) {
+//       dfs(r.right);
+//     }
+//   };
+//   dfs(root!);
+
+//   return answer;
+// }
