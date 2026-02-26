6bffdb29e22e8c81bf612d43f1e41cac7faf3498
hyunwoo <khj930410@naver.com>
2026-02-26 17:19:36 +0900

    feat: add solution for LeetCode problem 700 (search in a binary search tree)

---
+++ leetcode/easy/700.ts
@@ -0,0 +1,46 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  let answer: TreeNode | null = null;
+  const dfs = (r: TreeNode) => {
+    if (r.val === val) {
+      answer = r;
+      return;
+    }
+
+    if (r.left) {
+      dfs(r.left);
+    }
+    if (r.right) {
+      dfs(r.right);
+    }
+  };
+  dfs(root!);
+
+  return answer;
+}
