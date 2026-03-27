ab99cf5973bdb17240bf1afa32e35efde28812c4
hyunwooKwon <khj930410@naver.com>
2026-03-27 12:24:08 +0900

    feat: add solution for LeetCode problem 3349 (adjacent increasing subarrays detection)

---
+++ leetcode/easy/3349.ts
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/adjacent-increasing-subarrays-detection-i/description/
+
+
+//Constraints:
+//2 <= nums.length <= 100
+//1 < 2 * k <= nums.length
+//−1000 <= nums[i] <= 1000
+function hasIncreasingSubarrays(nums: number[], k: number): boolean {
+    if(k===1){
+        return true
+    }
+    
+    let queue: number[] = [nums[0]]
+    for(let i=1; i<=nums.length-1-k; ++i){
+        const n = nums[i]
+        const last = queue.at(-1)
+        if(last!==undefined && last>=n){
+            queue = []
+        }
+        queue.push(n)
+        
+        if(queue.length===k && i+k < nums.length){
+            const pairSub = [nums[i+1]] 
+            for(let j=i+2; j<=i+k ; ++j){
+                const n2 = nums[j]
+                const last2 = pairSub.at(-1)
+                if(last2!==undefined && last2>=n2){
+                    break
+                }
+                pairSub.push(n2)
+            }
+            if(pairSub.length===k){
+                return true
+            }
+            queue.shift()
+        }   
+    }
+    return false
+};
