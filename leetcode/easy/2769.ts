0e6fad5525f7f63962b094ed7c02e407b04597ed
lbaku89 <khj930410@naver.com>
2026-07-19 18:30:11 +0900

    feat: solve LeetCode 2769

---
+++ leetcode/easy/2769.ts
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+function theMaximumAchievableX(num: number, t: number): number {
+
+  return num + 2*t
+
+  // num-1 ~ num+1
+  // t-1 ~ t+1  
+};
