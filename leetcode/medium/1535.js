0723ead11d14694614d3a358e54834fb7002c599
hyunwoo <khj930410@naver.com>
2026-08-22 12:41:37 +0900

    feat: add solution for LeetCode problem 1535 - Find the Winner of an Array Game

---
+++ leetcode/medium/1535.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var getWinner = function (arr, k) {
+  let max = arr[0];
+  let consecutive = 0;
+
+  for (let i = 1; i < arr.length; ++i) {
+    if (max > arr[i]) {
+      ++consecutive;
+    } else {
+      consecutive = 1;
+      max = arr[i];
+    }
+
+    if (consecutive >= k) {
+      return max;
+    }
+  }
+  return max;
+};
