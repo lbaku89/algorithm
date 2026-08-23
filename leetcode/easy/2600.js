0ef3ccb780a320ddb73a815c7e55836a5aca6ad6
hyunwoo <khj930410@naver.com>
2026-08-23 10:44:38 +0900

    feat: add solution for LeetCode problem 2600 - K Items With the Maximum Sum

---
+++ leetcode/easy/2600.js
@@ -0,0 +1,22 @@
+/**
+ * @source https://leetcode.com/problems/k-items-with-the-maximum-sum/
+ */
+
+/**
+ * @param {number} numOnes
+ * @param {number} numZeros
+ * @param {number} numNegOnes
+ * @param {number} k
+ * @return {number}
+ */
+var kItemsWithMaximumSum = function (numOnes, numZeros, numNegOnes, k) {
+  if (k <= numOnes) {
+    return k;
+  }
+
+  if (k <= numOnes + numZeros) {
+    return numOnes;
+  }
+
+  return numOnes - (k - numOnes - numZeros);
+};
