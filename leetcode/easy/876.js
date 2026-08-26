8b3598d113f78887c41526154fde6f4197b3aea7
hyunwoo <khj930410@naver.com>
2026-08-26 10:15:43 +0900

    feat: add optimized solution for LeetCode problem 876 - Middle of the Linked List

--- leetcode/easy/876.js
+++ leetcode/easy/876.js
@@ -30,3 +30,25 @@ var middleNode = function (head) {
     node = node.next;
   }
 };
+
+/**
+ * Definition for singly-linked list.
+ * 정석 풀이
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
