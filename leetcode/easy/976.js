0de4b1492ce833186ccdd86e7d365d1e14757200
hyunwoo <khj930410@naver.com>
2026-09-15 10:45:17 +0900

    feat: solve LeetCode problem 975

---
+++ leetcode/easy/976.js
@@ -0,0 +1,18 @@
+/**
+ * @problem https://leetcode.com/problems/largest-perimeter-triangle/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+  nums.sort((a, b) => b - a);
+
+  for (let i = 0; i <= nums.length - 3; ++i) {
+    if (nums[i] < nums[i + 1] + nums[i + 2]) {
+      return nums[i] + nums[i + 1] + nums[i + 2];
+    }
+  }
+  return 0;
+};
